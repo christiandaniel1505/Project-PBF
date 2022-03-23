aef56e11e880603795de4b677ce5b86d92229519
christiandaniel1505 <75988134+christiandaniel1505@users.noreply.github.com>
2022-03-23 17:28:24 +0700

    Project PBF

---
+++ 1/defaultParameters.js
@@ -0,0 +1,5 @@
+function welcome(user = 'Cantik' , message = 'Selamat beraktifitas dan Jangan lupa makan yah'){
+    alert(`Hai ${user}, ${message}`);
+}
+
+welcome();
---
+++ 1/template.js
@@ -0,0 +1,7 @@
+let fname = 'christian';
+let lname = 'Ronaldo';
+let age = prompt("masukan umur Christian Ronaldo!");
+
+let result = `${fname} ${lname} is ${age} years old`;
+alert(result);
+
